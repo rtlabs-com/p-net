08809131654e50bffb736afb3fd8694b9197c629
olabjorsne <ola.bjorsne@rt-labs.com>
2020-12-10 14:08:37 +0100

    Added support for multiple ports in pf_port_get_list_of_ports()

--- src/device/pf_port.c
+++ src/device/pf_port.c
@@ -26,7 +26,7 @@
  * @brief Manage runtime data for multiple ports
  */
 
-void pf_port_init(pnet_t * net)
+void pf_port_init (pnet_t * net)
 {
    int port;
    pf_port_iterator_t port_iterator;
@@ -43,14 +43,29 @@ void pf_port_init(pnet_t * net)
    }
 }
 
-void pf_port_get_list_of_ports (pnet_t * net, pf_lldp_port_list_t * p_list)
+void pf_port_get_list_of_ports (
+   pnet_t * net,
+   uint16_t max_port,
+   pf_lldp_port_list_t * p_list)
 {
-   /* TODO: Implement support for multiple ports */
+   uint16_t port;
+   uint16_t index = 0;
+   uint8_t port_bit_mask = 0;
    memset (p_list, 0, sizeof (*p_list));
-   p_list->ports[0] = BIT (8 - 1);
+
+   for (port = PNET_PORT_1; port <= max_port; port++)
+   {
+      /* See documentation of pf_lldp_port_list_t for details*/
+      index = (port - 1) / 8;
+      CC_ASSERT (index < sizeof (*p_list));
+      port_bit_mask = BIT (7 - ((port - 1) % 8));
+      p_list->ports[index] |= port_bit_mask;
+   }
 }
 
-void pf_port_init_iterator_over_ports (pnet_t * net, pf_port_iterator_t * p_iterator)
+void pf_port_init_iterator_over_ports (
+   pnet_t * net,
+   pf_port_iterator_t * p_iterator)
 {
    p_iterator->next_port = 1;
 }
@@ -71,10 +86,8 @@ int pf_port_get_next (pf_port_iterator_t * p_iterator)
    return ret;
 }
 
-pf_port_t * pf_port_get_state (
-   pnet_t * net,
-   int loc_port_num)
+pf_port_t * pf_port_get_state (pnet_t * net, int loc_port_num)
 {
-   CC_ASSERT(loc_port_num > 0 && loc_port_num <= PNET_MAX_PORT);
+   CC_ASSERT (loc_port_num > 0 && loc_port_num <= PNET_MAX_PORT);
    return &net->port[loc_port_num - 1];
 }
