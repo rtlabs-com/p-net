b79589f05aafd0ac47364c05eb78f82f26528619
Hans-Erik Floryd <hans-erik.floryd@rt-labs.com>
2020-12-11 09:51:47 +0100

    delivery for P20027 MSA
    Company: Bender GmbH & Co. KG
    Product: COMTRAXX
    Product description: COMTRAXX products provided by Bender GmbH & Co. KG
    Port: linux
    
    Change-Id: Ib6c786f252aa9224489699f64d4fa912f575b2cc

--- LICENSE.md
+++ LICENSE.md
@@ -1,16 +1,36 @@
-# License
+-----BEGIN PGP SIGNED MESSAGE-----
+Hash: SHA512
 
-This software is dual-licensed.
+p-net Profinet device stack
+===========================
 
-## GPL version 3
+Copyright 2018 rt-labs AB
 
-This software is distributed under GPLv3. You are allowed to use this
-software for an open-source project with a compatible license.
+This instance of p-net is licensed for use in a specific product, as
+follows:
 
-[GNU GPL license v3](https://www.gnu.org/licenses/gpl-3.0.html)
+Company: Bender GmbH & Co. KG
+Product: COMTRAXX
+Product description: COMTRAXX products provided by Bender GmbH & Co. KG
 
-## Commercial license
+Use of this instance of p-net in other products is not allowed.
 
-This software is also available under a commercial license with
-options for support and maintenance. Please contact sales@rt-labs.com
-for further details.
+You are hereby given a non-exclusive, non-transferable license to use
+the SOFTWARE and documentation according to the SOFTWARE LICENSE
+AGREEMENT document shipped together with this instance of the
+SOFTWARE. The SOFTWARE LICENSE AGREEMENT prohibits copying and
+redistribution of the SOFTWARE.  It prohibits reverse-engineering
+activities.  It limits warranty and liability on behalf of rt-labs.
+The details of these terms are explained in subsequent chapters of the
+SOFTWARE LICENSE AGREEMENT.
+-----BEGIN PGP SIGNATURE-----
+
+iQEzBAEBCgAdFiEE1e4U+gPgGJXLsxxLZlG0bopmiXIFAl/TMvQACgkQZlG0bopm
+iXJE1QgAlEKblKkIrLF+hVRm6I1IfbGV6VWnBx+9IPwaHZmgQZAVTOlp4RHiNAre
+Il1l1xheENWpuihbVlvz5hMCVto4fkT13IUXxOo9Va9MK8lm6JOu2lYpl1HgVIwV
+R6I6dsWLeNBSBaA38DDBHIfjP4LBZC/HVYO2ISc3/OKutqasz/WIp+AjAURiKRsf
+yaWGay88FITHymmqJpGaWzB+jAOvRFs8hW/GsOyX/r17AK5F5N51O+UqUTKAWNPT
+f6M8QF0b07Hw+eOuB7A/0D5nvSl/rOd9LvYkiUhIUmQdHqERNkF9EJdroIjxTMXi
+lmoOkPEqyGV58fh7JjMYlI60sPlGBw==
+=TaN+
+-----END PGP SIGNATURE-----
--- src/device/pf_diag.c
+++ src/device/pf_diag.c
@@ -1,18 +1,41 @@
+/*
+-----BEGIN PGP SIGNED MESSAGE-----
+Hash: SHA512
 
-/*********************************************************************
- *        _       _         _
- *  _ __ | |_  _ | |  __ _ | |__   ___
- * | '__|| __|(_)| | / _` || '_ \ / __|
- * | |   | |_  _ | || (_| || |_) |\__ \
- * |_|    \__|(_)|_| \__,_||_.__/ |___/
- * www.rt-labs.com
- * Copyright 2018 rt-labs AB, Sweden.
- *
- * This software is dual-licensed under GPLv3 and a commercial
- * license. See the file LICENSE.md distributed with this software for
- * full license information.
- * See LICENSE file in the project root for full license information.
- ********************************************************************/
+p-net Profinet device stack
+===========================
+
+Copyright 2018 rt-labs AB
+
+This instance of p-net is licensed for use in a specific product, as
+follows:
+
+Company: Bender GmbH & Co. KG
+Product: COMTRAXX
+Product description: COMTRAXX products provided by Bender GmbH & Co. KG
+
+Use of this instance of p-net in other products is not allowed.
+
+You are hereby given a non-exclusive, non-transferable license to use
+the SOFTWARE and documentation according to the SOFTWARE LICENSE
+AGREEMENT document shipped together with this instance of the
+SOFTWARE. The SOFTWARE LICENSE AGREEMENT prohibits copying and
+redistribution of the SOFTWARE.  It prohibits reverse-engineering
+activities.  It limits warranty and liability on behalf of rt-labs.
+The details of these terms are explained in subsequent chapters of the
+SOFTWARE LICENSE AGREEMENT.
+-----BEGIN PGP SIGNATURE-----
+
+iQEzBAEBCgAdFiEE1e4U+gPgGJXLsxxLZlG0bopmiXIFAl/TMvQACgkQZlG0bopm
+iXJE1QgAlEKblKkIrLF+hVRm6I1IfbGV6VWnBx+9IPwaHZmgQZAVTOlp4RHiNAre
+Il1l1xheENWpuihbVlvz5hMCVto4fkT13IUXxOo9Va9MK8lm6JOu2lYpl1HgVIwV
+R6I6dsWLeNBSBaA38DDBHIfjP4LBZC/HVYO2ISc3/OKutqasz/WIp+AjAURiKRsf
+yaWGay88FITHymmqJpGaWzB+jAOvRFs8hW/GsOyX/r17AK5F5N51O+UqUTKAWNPT
+f6M8QF0b07Hw+eOuB7A/0D5nvSl/rOd9LvYkiUhIUmQdHqERNkF9EJdroIjxTMXi
+lmoOkPEqyGV58fh7JjMYlI60sPlGBw==
+=TaN+
+-----END PGP SIGNATURE-----
+*/
 
 /**
  * @file
