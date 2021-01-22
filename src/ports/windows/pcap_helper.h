eca332edb32fd329ed78a0bdc895350c1542726d
Morten Kvistgaard <morten@kvistgaard.org>
2021-01-22 14:04:32 +0100

    It seems that I've got a carte blanche to mess around with official Rt-Labs logos.

--- src/ports/windows/pcap_helper.h
+++ src/ports/windows/pcap_helper.h
@@ -1,4 +1,13 @@
 /*********************************************************************
+ *        _       _         _
+ *  _ __ | |_  _ | |  __ _ | |__   ___
+ * | '__|| __|(_)| | / _` || '_ \ / __|
+ * | |   | |_  _ | || (_| || |_) |\__ \
+ * |_|    \__|(_)|_| \__,_||_.__/ |___/
+ *
+ * www.rt-labs.com
+ * Copyright 2018 rt-labs AB, Sweden.
+ *
  * This software is dual-licensed under GPLv3 and a commercial
  * license. See the file LICENSE.md distributed with this software for
  * full license information.
