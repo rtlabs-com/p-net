0c5b9068b950247da8e0b435b5c5e7345659527a
Hans-Erik Floryd <hans-erik.floryd@rt-labs.com>
2021-02-18 12:57:52 +0100

    Fix gtest warning on embedded targets
    Fix warning when running on targets without argc, argv.

--- test/pf_test.cpp
+++ test/pf_test.cpp
@@ -17,7 +17,11 @@
 
 int main (int argc, char * argv[])
 {
-   ::testing::InitGoogleTest (&argc, argv);
+   if (argc > 0)
+      ::testing::InitGoogleTest (&argc, argv);
+   else
+      ::testing::InitGoogleTest();
+
    int result = RUN_ALL_TESTS();
    return result;
 }
