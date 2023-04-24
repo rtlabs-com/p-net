c20744f870f8c23f400c0cbe0e9aa973cd95a1b6
Roald Ruiter <ruiter@inspire.ethz.ch>
2023-04-24 09:33:48 +0200

    move helper methods to separate file

--- samples/pn_setpoint_from_button/app_data.c
+++ samples/pn_setpoint_from_button/app_data.c
@@ -20,6 +20,7 @@
 #include "sampleapp_common.h"
 #include "osal.h"
 #include "pnal.h"
+#include "utils.h"
 #include <pnet_api.h>
 
 #include <stdio.h>
@@ -94,44 +95,6 @@ uint8_t * app_data_get_input_data (
 
    /* Automated RT Tester scenario 2 - unsupported (sub)module */
    return NULL;
-}
-
-bool are_arrays_equal (
-   const uint8_t arr1[],
-   int size1,
-   const uint8_t arr2[],
-   int size2)
-{
-   if (size1 != size2)
-   {
-      return false;
-   }
-
-   for (int i = 0; i < size1; i++)
-   {
-      if (arr1[i] != arr2[i])
-      {
-         return false;
-      }
-   }
-
-   return true;
-}
-
-uint32_t combine_bytes (
-   uint8_t byte1,
-   uint8_t byte2,
-   uint8_t byte3,
-   uint8_t byte4)
-{
-   uint32_t result = 0;
-
-   result |= (uint32_t)byte1 << 24;
-   result |= (uint32_t)byte2 << 16;
-   result |= (uint32_t)byte3 << 8;
-   result |= (uint32_t)byte4;
-
-   return result;
 }
 
 int app_data_set_output_data (
